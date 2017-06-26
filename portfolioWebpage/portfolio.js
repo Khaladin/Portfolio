d3b40e14827d1b0367254c2f8de4a0c001a3698d
Cory Hoban <hobancor@gmail.com>
2017-06-26 19:54:52 -0400

    Adding all files

---
+++ portfolioWebpage/portfolio.js
@@ -0,0 +1,9 @@
+
+
+var btn = document.createElement("button");
+var linked = document.createTextNode("LinkedIn");
+btn.appendChild(linked);
+document.getElementById("linkedIn").appendChild(btn);
+
+
+
